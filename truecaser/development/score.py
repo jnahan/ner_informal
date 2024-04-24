f7c67fcdab02f0fadb002f4fd91b4d4396fd7505
Emily Wang <egw7501@nyu.edu>
2024-04-24 15:11:14 -0400

    implemented recall, precision, and f-measure

--- truecaser/development/score.py
+++ truecaser/development/score.py
@@ -26,8 +26,16 @@ def score (keyFileName, responseFileName):
 		exit()
 	correct = 0
 	incorrect = 0
-	correct_is_title = 0
-	correct_is_lower = 0
+	correct_title = 0
+	correct_lower = 0
+	correct_upper = 0
+	total = 0
+	correct_title_incorrect_lower = 0
+	correct_title_incorrect_upper = 0
+	correct_lower_incorrect_title = 0
+	correct_lower_incorrect_upper = 0
+	correct_upper_incorrect_title = 0
+	correct_upper_incorrect_lower = 0
 	for i in range(len(key)):
 		key[i] = key[i].rstrip(os.linesep)
 		response[i] = response[i].rstrip(os.linesep)
@@ -52,30 +60,71 @@ def score (keyFileName, responseFileName):
 		if responseToken != keyToken:
 			print ("token mismatch at line " + str(i))
 			exit()
-
+		total = total + 1
 		if responsePos == keyPos:
-			correct = correct + 1
+			if keyPos == "title case":
+				correct_title = correct_title + 1
+			elif keyPos == "lowercase":
+				correct_lower = correct_lower + 1
+			elif keyPos == "uppercase":
+				correct_upper = correct_upper + 1
+			correct = correct+1
 		elif responsePos != keyPos:
 			incorrect = incorrect + 1
 
 			if keyPos == "title case":
-				correct_is_title += 1
+				if responsePos == "lowercase":
+					correct_title_incorrect_lower = correct_title_incorrect_lower+1
+				elif responsePos == "uppercase":
+					correct_title_incorrect_upper = correct_title_incorrect_upper+1
 			elif keyPos == "lowercase":
-				correct_is_lower += 1
+				if responsePos == "title case":
+					correct_lower_incorrect_title = correct_lower_incorrect_title+1
+				elif responsePos == "uppercase":
+					correct_lower_incorrect_upper = correct_lower_incorrect_upper+1
+			elif keyPos == "uppercase":
+				if responsePos == "lowercase":
+					correct_upper_incorrect_lower = correct_upper_incorrect_lower+1
+				elif responsePos == "title case":
+					correct_upper_incorrect_title = correct_upper_incorrect_title+1
 
 			incorrect_list = open("truecaser/testing/incorrect.pos", 'a')
 			incorrect_list.write('line ' + str(i) + '\n')
 			incorrect_list.write('answer: ' + keyToken.strip() + '  ' + keyPos.strip() + '\n')
 			incorrect_list.write('response: ' + responseToken.strip() + '  ' + responsePos.strip() + '\n\n')
 
-			print('answer: ' + keyToken.strip() + '  ' + keyPos.strip())
-			print('response: ' + responseToken.strip() + '  ' + responsePos.strip())
-			print('')
+			# print('answer: ' + keyToken.strip() + '  ' + keyPos.strip())
+			# print('response: ' + responseToken.strip() + '  ' + responsePos.strip())
+			# print('')
 	print (str(correct) + " out of " + str(correct + incorrect) + " tags correct")
 	accuracy = 100.0 * correct / (correct + incorrect)
-	print("  accuracy: %f" % accuracy)
-	print ("incorrect, answer was supposed to be title: " + str(correct_is_title))
-	print ("incorrect, answer was supposed to be lower: " + str(correct_is_lower))
+	# print ("incorrect, answer was supposed to be title: " + str(correct_is_title))
+	# print ("incorrect, answer was supposed to be lower: " + str(correct_is_lower))
+	print("accuracy: %f" % accuracy)
+
+	print()
+	print("Title Case Evaluation")
+	precision = 100.0*correct_title/(correct_title + correct_lower_incorrect_title + correct_upper_incorrect_title)
+	recall = 100.0*correct_title/(correct_title+correct_title_incorrect_lower+correct_title_incorrect_upper) 
+	print("Precision: %f" % precision)
+	print("Recall: %f" % recall)
+	print("F-Score: %f"% (2.0/((1.0/precision)+(1.0/recall))))
+
+	print()
+	print("Lowercase Evaluation")
+	precision = 100.0*correct_lower/(correct_lower + correct_title_incorrect_lower + correct_upper_incorrect_lower)
+	recall = 100.0*correct_lower/(correct_lower+correct_lower_incorrect_title+correct_lower_incorrect_upper) 
+	print("Precision: %f" % precision)
+	print("Recall: %f" % recall)
+	print("F-Score: %f"% (2.0/((1.0/precision)+(1.0/recall))))
+
+	print()
+	print("Uppercase Evaluation")
+	precision = 100.0*correct_upper/(correct_upper + correct_title_incorrect_upper + correct_lower_incorrect_upper)
+	recall = 100.0*correct_upper/(correct_upper+correct_upper_incorrect_title+correct_upper_incorrect_lower) 
+	print("Precision: %f" % precision)
+	print("Recall: %f" % recall)
+	print("F-Score: %f"% (2.0/((1.0/precision)+(1.0/recall))))
 
 def main(args):
 	key_file = args[1]
