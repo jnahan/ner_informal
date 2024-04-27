5cd335a1035de2d586e3091bcfbffe913fe7823c
Emily Wang <egw7501@nyu.edu>
2024-04-26 23:03:25 -0400

    Revert "retokenized with spacy"
    This reverts commit f3b3d89c5e527451b7c43cedfbe8dcfbfa2e9ed1.

--- truecaser/formatter/corpora.py
+++ truecaser/formatter/corpora.py
@@ -1,80 +1,50 @@
 import nltk
 from nltk.corpus import gutenberg
+from nltk.tokenize import word_tokenize
 import re
-import spacy
-from nltk.tokenize.treebank import TreebankWordDetokenizer
 
-nlp = spacy.load("en_core_web_sm") 
-nlp.max_length = 1500000
 nltk.download('gutenberg')
-
-sentences = []
-sentence = []
-with open("truecaser/training/training.words",'r') as file:
-    for line in file:
-        if line=='\n':
-            sentences.append(TreebankWordDetokenizer().detokenize(sentence))
-            sentence = []
-        else:
-            sentence.append(line.strip())
-
-with open("truecaser/training/training.words",'w') as file:
-    for sentence in sentences:
-        doc = nlp(sentence)
-        for token in doc:
-            file.write(token.text+"\n")
-        file.write("\n")
+nltk.download('punkt')
 
 emma = gutenberg.raw('austen-emma.txt')[27:]
-tokens = nlp(emma)
-with open('truecaser/training/emma.words','w') as file:
+tokens = word_tokenize(emma)
+with open('truecaser/emma.words','w') as file:
     for token in tokens:
-        if token.text=="\n":
-            continue
-        text = token.text.strip()
-        file.write(text+'\n')
+        file.write(token+'\n')
+        if token in {'.', '?', '!'}:
+            file.write('\n')
 
 moby = gutenberg.raw('melville-moby_dick.txt')
 chapter_pattern = re.compile(r"CHAPTER 1|Chapter 1|chapter 1", re.IGNORECASE)
 match = chapter_pattern.search(moby)
 moby = moby[match.start():]
-tokens = nlp(moby)
-with open('truecaser/training/moby.words','w') as file:
+tokens = word_tokenize(moby)
+with open('truecaser/moby.words','w') as file:
     for token in tokens:
-        text = token.text.replace('\n', '').strip()
-        if text == "":
-            continue
-        file.write(text + '\n')
-        if text in {'.', '?', '!'}:
+        file.write(token+'\n')
+        if token in {'.', '?', '!'}:
             file.write('\n')
 
 persuasion = gutenberg.raw('austen-persuasion.txt')[33:]
-tokens = nlp(persuasion)
-with open('truecaser/training/persuasion.words','w') as file:
+tokens = word_tokenize(persuasion)
+with open('truecaser/persuasion.words','w') as file:
     for token in tokens:
-        if token.text=="\n":
-            continue
-        text = token.text.strip()
-        file.write(text+'\n')
+        file.write(token+'\n')
+        if token in {'.', '?', '!'}:
+            file.write('\n')
 
 parents = gutenberg.raw('edgeworth-parents.txt')[47:]
-tokens = nlp(parents)
-with open('truecaser/training/parents.words','w') as file:
+tokens = word_tokenize(parents)
+with open('truecaser/parents.words','w') as file:
     for token in tokens:
-        text = token.text.replace('\n', '').strip()
-        if text == "":
-            continue
-        file.write(text + '\n')
-        if text in {'.', '?', '!'}:
+        file.write(token+'\n')
+        if token in {'.', '?', '!'}:
             file.write('\n')
 
 alice = gutenberg.raw('carroll-alice.txt')[57:]
-tokens = nlp(alice)
-with open('truecaser/training/alice.words','w') as file:
+tokens = word_tokenize(alice)
+with open('truecaser/alice.words','w') as file:
     for token in tokens:
-        text = token.text.replace('\n', '').strip()
-        if text == "":
-            continue
-        file.write(text + '\n')
-        if text in {'.', '?', '!'}:
+        file.write(token+'\n')
+        if token in {'.', '?', '!'}:
             file.write('\n')
